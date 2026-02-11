919ff791131c7c7ecc6b1ce47085d38b162292af
Yang Zhanhao <3826144100@qq.com>
2026-02-11 09:01:37 +0000

    增加了/workspaces/Python-Quick-Learning-Guide/main.py。

---
+++ main.py
@@ -0,0 +1,28 @@
+from time import sleep
+
+def output(message, delay=3):
+    print(message)
+    sleep(delay)
+
+output("欢迎来到Python编程世界！")
+output("有关Python的更多信息，请访问官方网站：https://www.python.org/")
+output("开始你的第一个Python项目吧！", delay=0)
+
+output("要求：输出\"Hello, World!\"")
+output("示例代码：\n    print(\"Hello, World!\")", delay=0)
+omput_possibility = ["print(\"Hello, World!\")",
+                     "print('Hello, World!')",
+                     "print(\"Hello World!\")",
+                     "print('Hello World!')",
+                     "print(\"Hello,World!\")",
+                     "print('Hello,World!')",
+                     "print (\"Hello, World!\")",
+                     "print ('Hello, World!')",
+                     "print (\"Hello World!\")",
+                     "print ('Hello World!')",
+                     "print (\"Hello,World!\")",
+                     "print ('Hello,World!')"]
+string = input("试一试吧~\n1   ")
+while string not in omput_possibility:
+    string = input("输入错误，再试一次吧~\n1   ")
+output("恭喜你，完成了你的第一个Python程序！")
