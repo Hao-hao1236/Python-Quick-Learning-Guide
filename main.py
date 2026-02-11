238da1393b7c2dcd09d6c1263b5f4c8fe88992b4
Yang Zhanhao <3826144100@qq.com>
2026-02-11 09:37:31 +0000

    修改

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ output("欢迎来到Python编程世界！")
 output("有关Python的更多信息，请访问官方网站：https://www.python.org/")
 output("开始你的第一个Python项目吧！", delay=0)
 
-output("要求：输出\"Hello, World!\"")
-output("示例代码：\n    print(\"Hello, World!\")", delay=0)
-omput_possibility = ["print(\"Hello, World!\")",
-                     "print('Hello, World!')",
-                     "print(\"Hello World!\")",
-                     "print('Hello World!')",
-                     "print(\"Hello,World!\")",
-                     "print('Hello,World!')",
-                     "print (\"Hello, World!\")",
-                     "print ('Hello, World!')",
-                     "print (\"Hello World!\")",
-                     "print ('Hello World!')",
-                     "print (\"Hello,World!\")",
-                     "print ('Hello,World!')"]
+output("要求：输出\"Hello, world!\"")
+output("示例代码：\n    print(\"Hello, world!\")", delay=0)
+omput_possibility = ["print(\"Hello, world!\")",
+                     "print('Hello, world!')",
+                     "print(\"Hello world!\")",
+                     "print('Hello world!')",
+                     "print(\"Hello,world!\")",
+                     "print('Hello,world!')",
+                     "print (\"Hello, world!\")",
+                     "print ('Hello, world!')",
+                     "print (\"Hello world!\")",
+                     "print ('Hello world!')",
+                     "print (\"Hello,world!\")",
+                     "print ('Hello,world!')"]
 string = input("试一试吧~\n1   ")
 while string not in omput_possibility:
     string = input("输入错误，再试一次吧~\n1   ")
