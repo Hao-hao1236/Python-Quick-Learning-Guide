9ade982140c67b08b46e762178e449c8aee5e32e
Yang Zhanhao <3826144100@qq.com>
2026-02-12 10:04:10 +0000

    更新文件：/workspaces/Python-Quick-Learning-Guide/main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def output(message, delay=2):
 #                      "print (\"Hello,world!\")",
 #                      "print ('Hello,world!')"]
 
-# string = input("试一试吧~\n1   ").strip()
+# string = input("试一试吧~（尽量使用英文半角输入）\n1   ").strip()
 # while string not in output_possibility:
 #     string = input("输入错误，再试一次吧~\n1   ").strip()
 # output("=====RESTART: Python3 App=====\n" + \
@@ -51,7 +51,9 @@ while len(list) != 2 or list[0].strip() != 'age' or not list[1].strip().isdigit(
 
 string = input("2   ")
 list = string.split('=')
-while len(list) != 2 or list[0].strip() != 'name' or not list[1].strip().startswith('"') or not list[1].strip().endswith('"'):
+while len(list) != 2 or list[0].strip() != 'name' or \
+      (not list[1].strip().startswith('"') or not list[1].strip().endswith('"')) and \
+      (not list[1].strip().startswith("'") or not list[1].strip().endswith("'")):
     string = input("输入错误，再试一次吧~\n2   ")
     list = string.split('=')
 
@@ -60,3 +62,28 @@ list = string.split('=')
 while len(list) != 2 or list[0].strip() != 'is_student' or not list[1].strip().lower() in ['true', 'false']:
     string = input("输入错误，再试一次吧~\n3   ")
     list = string.split('=')
+
+output("真棒！你已经学会了如何在Python中定义变量和使用基本数据类型。")
+string = input("接下来试着用print()函数输出这些变量的值吧！（如果想要提示可直接按下回车键）\n4   ")
+
+def check_input(var_name):
+    if var_name == 'age':
+        output(25, delay=0)
+    elif var_name == 'name':
+        output("Alice", delay=0)
+    elif var_name == 'is_student':
+        output(True, delay=0)
+    else:
+        output("变量不存在哦~", delay=0)
+
+while True:  # 换种方法检查用户输入
+    if string == "":
+        output("示例代码：\n4   print(age)\n5   print(name)\n6   print(is_student)", delay=0)
+        string = input("4   ")
+        pass
+    elif string[0:5] == "print" and string.count('(') == 1 and string.count(')') == 1 and \
+        string.index(')') > string.index('(') + 1:
+        var_name = string[string.index('(') + 1:string.index(')')].strip()
+        check_input(var_name)
+    else:
+        string = input("输入错误，再试一次吧~\n4   ")
